d604be6b184443c0f97362c669640abfa69c1dbe
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2018-11-15 22:44:06 -0300

    movement prototype

--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeMovement : MonoBehaviour {
+public class SnakeMovement : MonoBehaviour
+{
 
     public float speed = 3.5f;
 
-    private 
+    private float turn = 0;
+    private Vector3 currentRotation, targetRotation;
 
-	void Start () {
-		
-	}
-	
-	void FixedUpdate () {
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        turn = Input.GetAxisRaw("Horizontal");
+        if (turn != 0)
+            Turn();
         MoveForward();
-	}
+    }
 
-    void MoveForward() {
+    void MoveForward()
+    {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void Turn()
+    {
+        currentRotation = transform.eulerAngles;
+        targetRotation.y = (currentRotation.y + (90 * turn));
+        transform.eulerAngles = targetRotation;
     }
 }
