d2ba639883fa15ced1df7054aa17e38a9a0547c2
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2018-11-15 22:02:15 -0300

    plane and head

---
+++ Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour {
+
+    public float speed = 3.5f;
+
+    private 
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        MoveForward();
+	}
+
+    void MoveForward() {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
